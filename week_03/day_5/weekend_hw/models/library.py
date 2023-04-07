db150d15dbb49020883b2f77ccb969cd95682564
Zoe Eustace <zoeeustace@zoes-air.home>
2023-04-07 13:44:13 +0100

    set up complete

---
+++ week_03/day_5/weekend_hw/models/library.py
@@ -0,0 +1,6 @@
+from models.book import *
+
+book1 = Book("The Great Gatsby", "F Scott Fitzgerald", "Literary Realism")
+book2 = Book("The Catcher in the Rye", "J D Sallinger", "Young Adult")
+book3 = Book("The Electric Kool-Aid Acid Test", "Tom Wolfe", "Nonfiction")
+books = [book1, book2, book3]
