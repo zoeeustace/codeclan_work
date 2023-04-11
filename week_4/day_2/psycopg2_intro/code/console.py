a67fc0261eb8c8326d50b442e0e1735d042d7c0a
Zoe Eustace <zoeeustace@Zoes-MacBook-Air.local>
2023-04-11 12:15:12 +0100

    console.py updated with user repo info

--- week_4/day_2/psycopg2_intro/code/console.py
+++ week_4/day_2/psycopg2_intro/code/console.py
@@ -2,23 +2,35 @@ import pdb
 from models.task import Task
 import repositories.task_repository as task_repository  
 from models.user import User
-import repositories.user_repository as user_repository
+from repositories import user_repository
 
-result = user_repository.select_all()
-
+user_repository.delete_all()
 task_repository.delete_all()
 
-task_1 = Task("Go for a run", "Jack Jarvis", 20)
+user_1 = User("Scooby", "Doo")
+user_repository.save(user_1)
+user_2 = User("Scrappy", "Doo")
+user_repository.save(user_2)
 
-task_repository.save(task_1)
+users = user_repository.select_all()
 
-task_1.mark_complete()
-task_repository.update(task_1)
+for user in users:
+    print(user.__dict__)
 
-result = task_repository.select_all()
 
-for task in result:
-    print(task.__dict__)
-#task.__dict__ allows results to be printed in dictionary format
 
-pdb.set_trace()
+
+# task_1 = Task("Go for a run", "Jack Jarvis", 20)
+
+# task_repository.save(task_1)
+
+# task_1.mark_complete()
+# task_repository.update(task_1)
+
+# result = task_repository.select_all()
+
+# for task in result:
+#     print(task.__dict__)
+# #task.__dict__ allows results to be printed in dictionary format
+
+# pdb.set_trace()
