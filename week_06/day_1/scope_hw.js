ef2190dd939dff5967cdba2c674f3396882f4884
Zoe Eustace <zoeeustace@Zoes-MacBook-Air.local>
2023-04-24 19:02:54 +0100

    uncommented some code for clarity of reading

--- week_06/day_1/scope_hw.js
+++ week_06/day_1/scope_hw.js
@@ -1,52 +1,52 @@
 // Episode 1
 
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Library',
-//   weapon: 'Rope'
-// };
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
 
-// const declareMurderer = function() {
-//   return `The murderer is ${scenario.murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
 
-// const verdict = declareMurderer();
-// console.log(verdict);
+const verdict = declareMurderer();
+console.log(verdict);
 
 // Miss Scarlet
 
 // Episode 2
 
-// const murderer = 'Professor Plum';
+const murderer = 'Professor Plum';
 
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
 
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // Professor Plum, as a const can't be changed (incorrect - creates an error)
 
 // Episode 3
 
-// let murderer = 'Professor Plum';
+let murderer = 'Professor Plum';
 
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
 
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
 
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
 
 // First Verdict: The murderer is Mrs Peacock
 // Second Verdict: The murderer is Professor Plum
@@ -54,18 +54,18 @@
 
 // Episode 4
 
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
 
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
 
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
 // The suspects are Miss Scarlet, Professor Plum, Colonel Mustard
 // Suspect three is Mrs Peacock
@@ -74,119 +74,119 @@
 
 // Episode 5
 
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Kitchen',
-//   weapon: 'Candle Stick'
-// };
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
 
-// const changeWeapon = function(newWeapon) {
-//   scenario.weapon = newWeapon;
-// }
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
 
-// const declareWeapon = function() {
-//   return `The weapon is the ${scenario.weapon}.`;
-// }
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
 
-// changeWeapon('Revolver');
-// const verdict = declareWeapon();
-// console.log(verdict);
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
 
 // The weapon is the Revolver
 // Able to change mutable values within a const object, such as a value to a key
 
 // Episode 6
 
-// let murderer = 'Colonel Mustard';
+let murderer = 'Colonel Mustard';
 
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
 
-//   const plotTwist = function() {
-//     murderer = 'Mrs. White';
-//   }
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
 
-//   plotTwist();
-// }
+  plotTwist();
+}
 
-// const declareMurderer = function () {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // The murderer is Mrs White - not completely sure on my reasoning here, but feel as though the plot twist is able
 // to happen because the function is nested and then run?
 
 // Episode 7
 
-// let murderer = 'Professor Plum';
+let murderer = 'Professor Plum';
 
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
 
-//   const plotTwist = function() {
-//     let murderer = 'Colonel Mustard';
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
 
-//     const unexpectedOutcome = function() {
-//       murderer = 'Miss Scarlet';
-//     }
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
 
-//     unexpectedOutcome();
-//   }
+    unexpectedOutcome();
+  }
 
-//   plotTwist();
-// }
+  plotTwist();
+}
 
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // The murderer is Mr Green - again, not too sure on my own reasoning here but something to do with having 
 // the let within the const for the plot twist?
 
 // Episode 8
 
-// const scenario = {
-//   murderer: 'Mrs. Peacock',
-//   room: 'Conservatory', //dining
-//   weapon: 'Lead Pipe'
-// };
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory', //dining
+  weapon: 'Lead Pipe'
+};
 
-// const changeScenario = function() {
-//   scenario.murderer = 'Mrs. Peacock';
-//   scenario.room = 'Dining Room';
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
 
-//   const plotTwist = function(room) {
-//     if (scenario.room === room) {
-//       scenario.murderer = 'Colonel Mustard';
-//     }
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
 
-//     const unexpectedOutcome = function(murderer) {
-//       if (scenario.murderer === murderer) {
-//         scenario.weapon = 'Candle Stick';
-//       }
-//     }
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
 
-//     unexpectedOutcome('Colonel Mustard');
-//   }
+    unexpectedOutcome('Colonel Mustard');
+  }
 
-//   plotTwist('Dining Room');
-// }
+  plotTwist('Dining Room');
+}
 
-// const declareWeapon = function() {
-//   return `The weapon is ${scenario.weapon}.`
-// }
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
 
-// changeScenario();
-// const verdict = declareWeapon();
-// console.log(verdict);
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
 
 // The weapon is Candle Stick
 // Because the dining room is passed in, then Colonel Mustard, then candle stick following the order of the if statements
