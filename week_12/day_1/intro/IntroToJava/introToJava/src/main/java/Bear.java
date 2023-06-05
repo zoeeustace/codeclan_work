7f83a9fa13ebaef05ffd806ea116f5a278ec128a
zoe <zoeeustace@hotmail.com>
2023-06-05 11:03:55 +0100

    cat/dog/bear/runner class

---
+++ week_12/day_1/intro/IntroToJava/introToJava/src/main/java/Bear.java
@@ -0,0 +1,11 @@
+public class Bear {
+    private String name;
+
+    public Bear(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
